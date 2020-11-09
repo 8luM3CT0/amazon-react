2256251b07b1d96ddb571d63bee27030d8bcd98c
Dusan Neme <rumlowb@gmail.com>
2020-11-08 23:39:54 -0500

    Updates

---
+++ precache-manifest.e7b6a180d1896c201678feac71a2710c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dec52ff50f6f3aa26615f6bbfa1cb51",
+    "url": "/amazon-react/index.html"
+  },
+  {
+    "revision": "4cee1c3c6da18365525b",
+    "url": "/amazon-react/static/css/main.7b4c1725.chunk.css"
+  },
+  {
+    "revision": "3bcf4c366a51c3134640",
+    "url": "/amazon-react/static/js/2.31886a0a.chunk.js"
+  },
+  {
+    "revision": "5ac922b1a6e253946f13fa455dc8d57c",
+    "url": "/amazon-react/static/js/2.31886a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cee1c3c6da18365525b",
+    "url": "/amazon-react/static/js/main.e2c76da9.chunk.js"
+  },
+  {
+    "revision": "bdc98bf2678874fd879f",
+    "url": "/amazon-react/static/js/runtime-main.fb613247.js"
+  }
+]);
